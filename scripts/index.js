a0d27e51d71035c0dff1afa1e54e4a1e9180b793
Daud Tariq <swift1supply2@gmail.com>
2024-09-24 10:03:37 -0400

    Part 2 finished

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const initialCards = [
+    { name: 'Karachi', link: 'https://tinyurl.com/ybwpy867' },
+    { name: 'Islamabad', link: 'https://tinyurl.com/mryp5wmp' },
+    { name: 'Lahore', link: 'https://tinyurl.com/yeywsd4p' },
+    { name: 'Multan', link: 'https://tinyurl.com/494k4dnk' },
+    { name: 'Peshawar', link: 'https://tinyurl.com/msmcp42f' },
+    { name: 'Abbottabad ', link: 'https://tinyurl.com/52mc289t' }
+]
